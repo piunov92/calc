fe9f703ce93749b1292cf846693ba760fa86c2d1
Sergey Piunov <piunov92@gmail.com>
2024-06-15 22:38:30 +0300

    fix trows Exception

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter expression: ");
-        System.out.println(calc(in.nextLine()));
+        try {
+            System.out.println(calc(in.nextLine()));
+        } catch (Exception e) {
+            System.out.printf(e.getMessage());
+        }
         in.close();
     }
 
