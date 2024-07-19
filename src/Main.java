a1d5812b6a02f8f9e4940ab8cdbb57799805d0af
Sergey Piunov <piunov92@gmail.com>
2024-07-19 14:23:05 +0300

    add doc

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main {
+    /**
+     *
+     * @param args
+     * @author Sergey Piunov
+     *
+     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter expression: ");
